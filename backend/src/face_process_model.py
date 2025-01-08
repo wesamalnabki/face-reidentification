b8afebaf038b5c136030c49c5a8de1d22b4d1909
walnabki <wesam.alnabki@gmail.com>
2025-01-08 09:52:33 +0100

    Add DeepFace

--- backend/src/face_process_model.py
+++ backend/src/face_process_model.py
@@ -1,8 +1,12 @@
 import insightface
 import numpy as np
+import requests
+
+from backend.src.utils import numpy_to_base64
 
 __all__ = [
-    'FaceProcess'
+    'FaceProcess',
+    "FaceProcess_DeepFace"
 ]
 
 
@@ -21,5 +25,28 @@ class FaceProcess:
             return None
             # raise HTTPException(status_code=400, detail="No face detected in the image.")
         face_embedding = faces[0].normed_embedding
+        # face_embedding: object = face_embedding / np.linalg.norm(face_embedding)
+        return face_embedding
+
+
+class FaceProcess_DeepFace:
+    def __init__(self):
+        self.url_path = "http://localhost:5000/represent"
+
+    # Utility functions
+    def get_face_embedding(self, image_np: np.array):
+        img_base64 = numpy_to_base64(image_np)
+
+        data = {
+            "model_name": "VGG-Face",
+            "align": True,
+            "max_faces": "1",
+            "silent": True,
+            "enforce_detection": False,
+            "img_path": "data:image/png;base64, " + img_base64,
+        }
+
+        response = requests.post(self.url_path, json=data)
+        face_embedding = np.array(response.json()['results'][0]['embedding'])
         # face_embedding: object = face_embedding / np.linalg.norm(face_embedding)
         return face_embedding
--- backend/src/utils.py
+++ backend/src/utils.py
@@ -1,4 +1,34 @@
+import base64
 import os
+from io import BytesIO
+
+from PIL import Image
+
+
+def numpy_to_base64(image_array, format="PNG"):
+    """
+    Convert a NumPy array (representing an image) to a base64-encoded string.
+
+    Args:
+        image_array (numpy.ndarray): The image as a NumPy array.
+        format (str): The image format (e.g., "PNG", "JPEG"). Default is "PNG".
+
+    Returns:
+        str: The base64-encoded string representation of the image.
+    """
+    # Convert the NumPy array to a PIL Image
+    image = Image.fromarray(image_array)
+
+    # Create a BytesIO object to hold the image data
+    img_byte_arr = BytesIO()
+
+    # Save the PIL Image to the BytesIO object in the specified format
+    image.save(img_byte_arr, format=format)
+
+    # Encode the image bytes to a base64 string
+    img_base64 = base64.b64encode(img_byte_arr.getvalue()).decode("utf-8")
+
+    return img_base64
 
 
 def save_image(person_id, face_id, image_pil):
